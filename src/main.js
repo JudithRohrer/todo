87d328b56151a133a0ab1ea0e6505916e2209e37
JudithRohrer <59763566+JudithRohrer@users.noreply.github.com>
2020-07-11 16:29:35 +0200

    deploy to gh pages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueMaterial from 'vue-material';
+import { MdField, MdCard, MdButton } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+
+Vue.use(VueMaterial);
+
+Vue.use(MdField);
+Vue.use(MdCard);
+Vue.use(MdButton);
+
 Vue.config.productionTip = false
 
 new Vue({
