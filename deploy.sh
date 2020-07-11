2d8210445bc8a06aaf939e04cbd0e852a01b054d
JudithRohrer <59763566+JudithRohrer@users.noreply.github.com>
2020-07-11 18:11:25 +0200

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,25 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/todo/'
-    : '/'
-}
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:JudithRohrer/todo.git master:gh-pages
+
+cd -
